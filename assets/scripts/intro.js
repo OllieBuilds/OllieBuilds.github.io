8909e24b99bea101b2d676a6fb80ad07fb70d24a
OllieBuilds <scarborough.oliver@gmail.com>
2016-04-08 22:19:24 -0400

    Walks through to "New Game" for Player one

--- assets/scripts/intro.js
+++ assets/scripts/intro.js
@@ -3,5 +3,14 @@
 $('#trip').on('click', function() {
   $('#land').addClass('hidden');
     console.log('test');
-  $('#register').removeClass('hidden');  
+  $('#register').removeClass('hidden');
+});
+
+$('#begin').on('click', function() {
+  $('#register').addClass('hidden');
+  $('#choose').removeClass('hidden');
+});
+
+$('#change-pw').on('click', function() {
+  $('#pw-change-success').removeClass('hidden');
 });
