ae8fcb219d3da0e4327200b2fc03ac5e1bd71afc
OllieBuilds <scarborough.oliver@gmail.com>
2016-04-11 15:08:25 -0400

    appears to have fixed game logic

--- assets/scripts/gameplay.js
+++ assets/scripts/gameplay.js
@@ -5,89 +5,47 @@
 // const ui = require('./auth/ui');
 //
 
-let xArray = ["", "", "", "", "", "", "", "", ""];
-let oArray = ["", "", "", "", "", "", "", "", ""];
+let xArray = [];
+let oArray = [];
 
 // win counters
   let playerXwins = 0;
   let playerOwins = 0;
   let numTurn = 0;
-  let currentPlayer;
-
+  // let mathWinner = 1;
 
 
 // announceWinner
 
 const announceWinner = function (){
-  console.log("annouce wins!");
+  console.log("announce wins!");
   if(numTurn % 2 === 0){
     playerXwins++;
     return playerXwins;
   }else {
+    playerOwins++;
     return playerOwins++;
   }
 };
 
 // Game Logic
+// const toMath = function(array) {
+//   let mathWinner = 1;
+//   for(let i = 0; i <array.length; i++){
+//     mathWinner *= array[i];
+//   }
+//   return mathWinner;
+// };
 
-const winCases = function(array){
-  console.log('win cases running');
-  for (let i = 0; i < array.length; i++){
-  // global --> top row 0
-if (array[0] !== ''){
-  // top row: horizontal
-  if(array[0] === array[1] === array[2]){
-announceWinner();
+const winCases = function(array) {
+  let mathWinner = 1;
+  for(let i = 0; i <array.length; i++){
+    mathWinner *= array[i]+1;
+  console.log(mathWinner);
+  if(mathWinner === 6 || mathWinner === 28 || mathWinner === 45 || mathWinner === 80 || mathWinner === 162 || mathWinner === 105 || mathWinner === 120 || mathWinner === 504){
+    announceWinner();
   }
-  // top row: vertical
-  else if(array[0] === array[3] === array[6]){
-    return announceWinner();
-  }
-  // top left diagonal
-  else if(array[0] === array[4] === array[8]){
-    return announceWinner();
-  }
-  //global --> 1
-}else if(array[1] !== ''){
-  // center column vertical
-  if(array[1] === array[4] === array[7]){
-    return announceWinner();
-  }
-  // global --> 2
-}else if(array[2] !== ''){
-  // right column vertical
-  if(array[2] === array[5] === array[8]){
-    return announceWinner();
-  }
-  // right corner diagonal
-  if(array[2] === array[4] === array[6]){
-    return announceWinner();
-  }
-  // global --> 3
-}else if(array[3] !== ''){
-  // middle row horizontal
-  if(array[3] === array[4] === array[5]){
-    return announceWinner();
-  }
-  // global --> 6
-}else if(array[6] !== ''){
-  // bottom row horizontal
-  if(array[6] === array[7] === array[8]){
-    return announceWinner();
-  }
-  // global
-  }
- }  // end function
- console.log('ran winCases');
-};
-
-const defined = function (array, identify){
-    let indexValue = identify.charAt(6);
-    array[+indexValue] = currentPlayer;
-    console.log(array);
-    let newArray = array;
-    console.log(newArray);
-    winCases(newArray);
+}
 };
 
 
@@ -95,28 +53,32 @@ const defined = function (array, identify){
 $('.square').on('click', function(){
   if(numTurn % 2 === 0){
     $(this).text('X');
-    let id = $(this).attr('id');
-    // xArray.push(id);
-    // console.log(gameArray);
-    numTurn++;
-    currentPlayer = 'X';
+    // xArray[$(this.attr('id')] = $(this).attr('id');
+    // xArray.push($(this).attr('id').charAt(6).parseInt());
+    let integer = $(this).attr('id').charAt(6);
+    let toNum = parseInt(integer);
+    xArray.push(toNum);
+    // toMath(xArray);
     console.log(xArray);
-    defined(xArray, id);
+    winCases(xArray);
+    numTurn++;
   }else{
     $(this).text('O');
-    let id = $(this).attr('id');
-    // oArray.push(id);
-    // console.log(gameArray);
+    // let id = $(this).attr('id');
+    let integer = $(this).attr('id').charAt(6);
+    let toNum = parseInt(integer);
+    oArray.push(toNum);
+    // toMath(oArray);
+    console.log(oArray);
+    winCases(oArray);
     numTurn++;
-    currentPlayer = 'O';
-    defined(oArray, id);
   }
 });
 
 module.exports = {
   announceWinner,
   winCases,
-  defined,
+  // defined,
   playerOwins,
   playerXwins
 
